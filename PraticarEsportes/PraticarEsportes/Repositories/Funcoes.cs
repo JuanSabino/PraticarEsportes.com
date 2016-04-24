5b9a7f804c05d09f6f78da455563c5f657684eaf
Juan Felipe Sabino Pereira <juan.pereira@uol.com.br>
2016-04-24 18:52:02 -0300

    US: recuperar senha

--- PraticarEsportes/PraticarEsportes/Repositories/Funcoes.cs
+++ PraticarEsportes/PraticarEsportes/Repositories/Funcoes.cs
@@ -2,6 +2,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Web;
 using System.Web.Security;
 
@@ -42,7 +44,7 @@ namespace PraticarEsportes.Repositories
         {
             string _login = HttpContext.Current.User.Identity.Name;
             int _tipo;
-            if (HttpContext.Current.Session.Count > 0 || HttpContext.Current.Session["Usuario"] != null)
+            if (HttpContext.Current.Session.Count > 0 && HttpContext.Current.Session["Usuario"] != "")
             {
                 _login = HttpContext.Current.Session["Usuario"].ToString();
                 if (_login == "")
@@ -111,6 +113,38 @@ namespace PraticarEsportes.Repositories
             historico.Descricao = Descricao;
             db.Historicos.Add(historico);
             db.SaveChanges();
+        }
+
+        //criptografia
+        public static string Hash(string Valor)
+        {
+            using (MD5 md5Hash = MD5.Create())
+            {
+                string hash = GetMd5Hash(md5Hash, Valor);                
+
+                return hash;
+            }
+        }
+
+        static string GetMd5Hash(MD5 md5Hash, string input)
+        {
+
+            // Convert the input string to a byte array and compute the hash.
+            byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+            // Create a new Stringbuilder to collect the bytes
+            // and create a string.
+            StringBuilder sBuilder = new StringBuilder();
+
+            // Loop through each byte of the hashed data 
+            // and format each one as a hexadecimal string.
+            for (int i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+
+            // Return the hexadecimal string.
+            return sBuilder.ToString();
         }
 
 
---
+++ PraticarEsportes/PraticarEsportes/Repositories/GmailEmailService.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Web;
+
+namespace PraticarEsportes.Repositories
+{
+    
+    public interface IEmailService
+    {
+        bool SendEmailService(EmailMessage message);
+    }
+
+    
+
+    public class SmtpConfiguration
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Host { get; set; }
+        public int Port { get; set; }
+        public bool Ssl { get; set; }
+    }
+
+    public class EmailMessage
+    {
+        public string ToEmail { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+        public bool isHtml { get; set; }
+    }
+
+    public class GmailEmailService : IEmailService
+    {
+        private readonly SmtpConfiguration _config;
+
+        public GmailEmailService()
+        {
+            _config = new SmtpConfiguration();
+            var gmailUserName = "praticaresportes.com@gmail.com";
+            var gmailPassword = "pi4semestre";
+            var gmailHost = "smtp.gmail.com";
+            var gmailPort = 587;
+            var gmailSsl = true;
+            _config.Username = gmailUserName;
+            _config.Password = gmailPassword;
+            _config.Host = gmailHost;
+            _config.Port = gmailPort;
+            _config.Ssl = gmailSsl;
+        }
+
+        public bool SendEmailService(EmailMessage message)
+        {
+            var success = false;
+            try
+            {
+                var smtp = new SmtpClient
+                {
+                    Host            = _config.Host,
+                    Port            = _config.Port,
+                    EnableSsl       = _config.Ssl,
+                    DeliveryMethod  = SmtpDeliveryMethod.Network,
+                    UseDefaultCredentials = false,
+                    Credentials = new NetworkCredential(_config.Username, _config.Password)
+                };
+                using (var smtpMessage = new MailMessage(_config.Username, message.ToEmail))
+                {
+                    smtpMessage.Subject = message.Subject;
+                    smtpMessage.Body = message.Body;
+                    smtpMessage.IsBodyHtml = message.isHtml;
+                    smtp.Send(smtpMessage);
+                }
+                success = true;
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return success;
+        }
+    }
+}
